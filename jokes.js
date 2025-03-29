8a13bb4f5f0bbde8b711399cfe28abf0a4a82536
DMAlexandra08 <Alexandra.Armstrong@edu.sait.ca>
2025-03-28 20:45:05 -0600

    html & js programming
    added programming js, with a lot of trial and error. clarified category title of title and joke elements, so they would update when clicked. weren't working prior, console log was giving error messages

--- jokes.js
+++ jokes.js
@@ -1,3 +1,6 @@
+const categoryTitle =document.getElementById('title');
+const joke =document.getElementById('joke');
+
 fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
 .then(response => response.json())
 .then(data => {
@@ -10,6 +13,23 @@ fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
     }
 });
 
-programming.addEventListener('click', function (){
-    console.log()
+programming.addEventListener('click', function () {
+    console.log("programming joke");
+    categoryTitle.textContent = "A Random Programming Joke 💻";
+    fetch("https://v2.jokeapi.dev/Programming?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if(data.type === "single") {
+            joke.textContent = data.joke;
+        }
+        else if(data.type === "twopart") {
+            joke.textContent = data.setup + "..." + data.delivery;
+        }
+    });
+});
+
+misc.addEventListener('click', function () {
+    console.log("misc joke");
+    
 })
