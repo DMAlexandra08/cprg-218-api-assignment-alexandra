bcf2ac4ab4a237fe60575f4c2aba5c50f682f36f
DMAlexandra08 <Alexandra.Armstrong@edu.sait.ca>
2025-03-28 21:10:22 -0600

    spooky & christmas js
    was able to copy & paste the programming joke & edit the words, saved a lot of typing. also realized i forgot the "joke" in the url so the jokes weren't changing.

--- jokes.js
+++ jokes.js
@@ -16,23 +16,7 @@ fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
 programming.addEventListener('click', function () {
     console.log("programming joke");
     categoryTitle.textContent = "A Random Programming Joke 🖥️";
-    fetch("https://v2.jokeapi.dev/Programming?safe-mode")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        if(data.type === "single") {
-            joke.textContent = data.joke;
-        }
-        else if(data.type === "twopart") {
-            joke.textContent = data.setup + "..." + data.delivery;
-        }
-    });
-});
-
-misc.addEventListener('click', function () {
-    console.log("misc joke");
-    categoryTitle.textContent = "A Random Miscellaneous Joke 🥸";
-    fetch("https://v2.jokeapi.dev/Miscellaneous?safe-mode")
+    fetch("https://v2.jokeapi.dev/joke/Programming?safe-mode")
     .then(response => response.json())
     .then(data => {
         console.log(data);
@@ -45,19 +29,66 @@ misc.addEventListener('click', function () {
     });
 });
 
-pun.addEventListener('click', function() {
-    console.log("pun joke");
-    categoryTitle.textContent = "A Random Punny Joke 🤓";
-    fetch("https://v2.jokeapi.dev/Pun?safe-mode")
+misc.addEventListener('click', function () {
+    console.log("misc joke");
+    categoryTitle.textContent = "A Random Miscellaneous Joke 🥸";
+    fetch("https://v2.jokeapi.dev/joke/Miscellaneous?safe-mode")
     .then(response => response.json())
-    .then(date => {
+    .then(data => {
         console.log(data);
-        if(date.type === "single"){
+        if(data.type === "single"){
             joke.textContent = data.joke;
         }
-        else if(date.type === "twopart"){
+        else if(data.type === "twopart") {
             joke.textContent = data.setup + "..." + data.delivery;
         }
     });
 });
 
+pun.addEventListener('click', function () {
+    console.log("pun joke");
+    categoryTitle.textContent = "A Random Punny Joke 🤓";
+    fetch("https://v2.jokeapi.dev/joke/Pun?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if(data.type === "single"){
+            joke.textContent = data.joke;
+        }
+        else if(data.type === "twopart") {
+            joke.textContent = data.setup + "..." + data.delivery;
+        }
+    });
+});
+
+spooky.addEventListener('click', function () {
+    console.log("spooky joke");
+    categoryTitle.textContent = "A Random Spooooky Joke 🎃";
+    fetch("https://v2.jokeapi.dev/joke/Spooky?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if(data.type === "single"){
+            joke.textContent = data.joke;
+        }
+        else if(data.type === "twopart") {
+            joke.textContent = data.setup + "..." + data.delivery;
+        }
+    });
+});
+
+christmas.addEventListener('click', function () {
+    console.log("christmas joke");
+    categoryTitle.textContent = "A Random Christmas Joke 🎅";
+    fetch("https://v2.jokeapi.dev/joke/Christmas/safe-mode")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if(data.type === "single"){
+            joke.textContent = data.joke;
+        }
+        else if(data.type === "twopart") {
+            joke.textContent = data.setup + "..." + data.delivery;
+        }
+    });
+});
