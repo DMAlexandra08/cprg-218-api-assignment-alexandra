bfc925bad7d6b1be91c229468f7289eb17745c73
DMAlexandra08 <Alexandra.Armstrong@edu.sait.ca>
2025-03-28 20:18:28 -0600

    html & js
    created p id 'joke' so that an actual joke would show up, and put the api for any joke in js when web loads

---
+++ jokes.js
@@ -0,0 +1,15 @@
+fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+    if(data.type === "single") {
+        joke.textContent = data.joke;
+    }
+    else if(data.type === "twopart") {
+        joke.textContent = data.setup + "..." + data.delivery;
+    }
+});
+
+programming.addEventListener('click', function (){
+    console.log()
+})
